7d6f7362301cd6ac04ce55b0f8b390b7409e2153
Jinesh Patel <jinesh701@gmail.com>
2018-01-17 16:45:57 -0500

    default blog items

---
+++ gameblogsJson.js
@@ -0,0 +1,14 @@
+db.gameblogs.insert([
+  {
+    title: "Uncharted 2",
+    content: "Amazing game!",
+    author: "Jinesh Patel",
+    completed: false
+  },
+  {
+    title: "Overwatch",
+    content: "Fun multiplayer game!!",
+    author: "Jinesh Patel",
+    completed: false
+  }
+]);
