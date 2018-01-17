a4ce588b60b779220cc6061dd0f1102bd359d8bd
Jinesh Patel <jinesh701@gmail.com>
2018-01-17 18:20:04 -0500

    default movie blogs

---
+++ movieblogsJson.js
@@ -0,0 +1,14 @@
+db.movieblogs.insert([
+  {
+    title: "Inception",
+    content: "Amazing movie!",
+    author: "Jinesh Patel",
+    watched: false
+  },
+  {
+    title: "The Dark Knight",
+    content: "Another amazing movie!",
+    author: "Jinesh Patel",
+    watched: false
+  }
+]);
