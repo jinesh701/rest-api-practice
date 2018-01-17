49dac525c247ad336778250019cbbdd7a0f5ac79
Jinesh Patel <jinesh701@gmail.com>
2018-01-17 18:20:15 -0500

    added GET

--- routers/movieBlogRouter.js
+++ routers/movieBlogRouter.js
@@ -6,6 +6,28 @@ mongoose.Promise = global.Promise;
 
 const MovieBlog = require("../models/movieblog");
 
-router.get("/", function(req, res) {});
+//GET
+router.get("/", function(req, res, next) {
+  MovieBlog.find()
+    .then(movieblogs =>
+      res.json(movieblogs.map(movieblog => movieblog.serialize()))
+    )
+    .catch(next);
+});
+
+//GET BY ID
+router.get("/:id", function(req, res, next) {
+  const id = req.params.id;
+
+  MovieBlog.findById(id)
+    .then(item => {
+      if (item) {
+        res.json(item.serialize());
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
 
 module.exports = router;
